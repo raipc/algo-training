61672d288651db5be240cc1e0dc97f9744b40571
raipc <anton.rybochkin@axibase.com>
2023-02-20 15:12:36 +0100

    Solved Container With Most Water

--- src/main/kotlin/org/raipc/leetcode/Solution.kt
+++ src/main/kotlin/org/raipc/leetcode/Solution.kt
@@ -1137,6 +1137,22 @@ class Solution {
         }
         return result.toInt()
     }
+
+    // 11. Container With Most Water
+    fun maxArea(height: IntArray): Int {
+        var result = 0
+        var left = 0
+        var right = height.lastIndex
+        while (left < right) {
+            result = maxOf(result, minOf(height[left], height[right]) * (right - left))
+            if (height[left] <= height[right]) {
+                left++
+            } else {
+                right--
+            }
+        }
+        return result
+    }
 }
 
 class ListNode(var `val`: Int) {
