2b42c80e303dfb714e9b99ddce71f9d2b7d9a088
raipc <anton.rybochkin@axibase.com>
2023-02-22 22:17:00 +0100

    Solved Fix Names in a Table

--- src/main/resources/leetcode_queries.sql
+++ src/main/resources/leetcode_queries.sql
@@ -28,3 +28,8 @@ UPDATE Salary SET sex = IF(sex='f','m','f')
 
 -- 196. Delete Duplicate Emails
 DELETE p1 FROM Person p1, Person p2 WHERE p1.email = p2.email AND p1.id > p2.id
+
+-- 1667. Fix Names in a Table
+SELECT user_id, CONCAT(UPPER(LEFT(name, 1)), LOWER(SUBSTRING(name, 2))) as name
+FROM Users
+ORDER BY user_id
