4744b29725aa000b4161e939b413b2db9eb59c7b
raipc <anton.rybochkin@axibase.com>
2023-02-23 23:57:42 +0100

    Solved Rearrange Products Table

--- src/main/resources/leetcode_queries.sql
+++ src/main/resources/leetcode_queries.sql
@@ -58,3 +58,16 @@ FROM (
     FROM Employees e RIGHT JOIN Salaries s USING(employee_id)
     WHERE e.name IS NULL) t
 ORDER BY 1
+
+-- 1795. Rearrange Products Table
+SELECT product_id, 'store1' AS store, store1 AS price
+FROM Products
+WHERE store1 IS NOT NULL
+UNION ALL
+SELECT product_id, 'store2' AS store, store2 AS price
+FROM Products
+WHERE store2 IS NOT NULL
+UNION ALL
+SELECT product_id, 'store3' AS store, store3 AS price
+FROM Products
+WHERE store3 IS NOT NULL
