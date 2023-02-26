93c13ec5e5717233c0d51d1a977c76f4ae35e100
raipc <anton.rybochkin@axibase.com>
2023-02-26 14:49:42 +0100

    Solved User Activity

--- src/main/resources/leetcode_queries.sql
+++ src/main/resources/leetcode_queries.sql
@@ -119,3 +119,9 @@ WHERE sales_id NOT IN (
     FROM Company JOIN Orders USING (com_id)
     WHERE name='RED'
 )
+
+-- 1141. User Activity for the Past 30 Days I
+SELECT activity_date AS day, COUNT(DISTINCT user_id) AS active_users
+FROM Activity
+WHERE activity_date < '2019-07-28' AND DATEDIFF('2019-07-27', activity_date) < 30
+GROUP BY activity_date
