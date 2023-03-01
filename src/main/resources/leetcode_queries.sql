25939c133dd3e5e74192678d3ae161e1bc842671
raipc <anton.rybochkin@axibase.com>
2023-03-01 23:00:28 +0100

    Solved Actors and Directors Who Cooperated At Least Three Times

--- src/main/resources/leetcode_queries.sql
+++ src/main/resources/leetcode_queries.sql
@@ -181,3 +181,9 @@ SELECT email
 FROM Person
 GROUP BY email
 HAVING COUNT(*) > 1
+
+-- 1050. Actors and Directors Who Cooperated At Least Three Times
+SELECT actor_id, director_id
+FROM ActorDirector
+GROUP BY actor_id, director_id
+HAVING COUNT(*) >= 3
