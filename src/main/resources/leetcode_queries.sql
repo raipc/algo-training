d846e228f6758e92ec8b23b6a4e91fcccb6927a0
raipc <anton.rybochkin@axibase.com>
2023-03-01 22:58:58 +0100

    Solved Duplicate Emails

--- src/main/resources/leetcode_queries.sql
+++ src/main/resources/leetcode_queries.sql
@@ -175,3 +175,9 @@ GROUP BY stock_name
 SELECT user_id AS buyer_id, join_date, IFNULL(COUNT(order_id), 0) AS orders_in_2019
 FROM Users LEFT JOIN Orders ON Users.user_id = Orders.buyer_id AND YEAR(order_date) = '2019'
 GROUP BY Users.user_id
+
+-- 182. Duplicate Emails
+SELECT email
+FROM Person
+GROUP BY email
+HAVING COUNT(*) > 1
